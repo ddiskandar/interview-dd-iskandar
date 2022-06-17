4e5502af4584b40af65ed367df3b506b25b1bccb
ddiskandar <dd@smkplusalfarhan.sch.id>
2022-06-17 08:15:19 +0700

    Update tenancy.php

--- config/tenancy.php
+++ config/tenancy.php
@@ -183,7 +183,7 @@ return [
      */
     'migration_parameters' => [
         '--force' => true, // This needs to be true to run migrations in production.
-        '--path' => [database_path('migrations/tenant')],
+        '--path' => [database_path('migrations/tenant'), 'Modules/Contact/Database/Migrations'],
         '--realpath' => true,
     ],
 
